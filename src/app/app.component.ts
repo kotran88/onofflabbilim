e9a28359b973de2355f1cc85cc0fc003921ed13a
MacBook <>
2020-03-20 18:14:23 +0900

    로그인 페이지 (당근 참조)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,8 @@ import { SignupPage } from '../pages/signup/signup';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  // rootPage:any = HomePage;
+  rootPage:any=LoginpagePage;
 
   constructor(platform: Platform/* , statusBar: StatusBar, splashScreen: SplashScreen */) {
     platform.ready().then(() => {
--- src/pages/loginpage/loginpage.ts
+++ src/pages/loginpage/loginpage.ts
@@ -20,182 +20,87 @@ import { SignupPage } from '../signup/signup';
 })
 export class LoginpagePage {
 
-  ValidateFlag:any=false;
-  id:any;
-  password:any;
-  check=false;
+  main_title='';
+
+  name='';
+  phone='';
+  code='';
+
+  certified_check=false;
+  login_check:any;
+  phone_check=false;
 
   firemain = firebase.database().ref();
   constructor(public alertCtrl:AlertController,public fire:AngularFireAuth,public navCtrl: NavController, public navParams: NavParams) {
-    localStorage.setItem("loginflag","false")
-    if(localStorage.getItem("loginflag")==='true'){
-      console.log('auto login');
-    }
-
-  
-    console.log(localStorage.getItem("loginflag"));
-
-  }
- 
-
-  confirmAlert(str){
-    var alert = this.alertCtrl.create({
-      subTitle: str,
-      buttons: ['확인']
-  });
-  alert.present({ animate: false });
-  }
-  find_admin(){
-  }
-  signup(){
-    this.navCtrl.push(SignupPage)
-  }
-  validate(){
-    var str = '';
-    if (this.id == '') {
-        str = '아이디를 입력해주세요.';
-        this.confirmAlert(str);
-        return false;
-    }
-    if (this.password == '') {
-        str = '비밀번호를 입력해주세요.';
-        this.confirmAlert(str);
-        return false;
-    }
-    this.ValidateFlag = true;
-    return true;
-  }
-  login(){
-    if (!this.validate()) {
-        return false;
-    }
-    if (this.ValidateFlag == true){
-      // this.login_success();
-    }
-        
-    console.log(this.id);
-    console.log(this.password);
-    this.fire.auth.signInWithEmailAndPassword(this.id, this.password)
-        .then( (data)=> {
-        this.login_success(this.id);
-    }).catch(function (error) {
-        console.log(error);
-        alert("login failed" + error);
-    });
-  }
-
-  login_success(id){
-    localStorage.setItem("loginflag","true")
-    localStorage.setItem("id",id)
-    console.log('login success')
-    console.log(id+'check',this.check)
     
-    // this.id=localStorage.getItem('id');
-    var key="";
-    var flag=false;
-    this.firemain.child("users").once("value",(snap)=>{
-      console.log(snap.val());
-      
-      for(var a in snap.val()){
-        console.log(snap.val()[a].email);
-        if(id==snap.val()[a].email){
-          flag=true;
-          key=snap.val()[a].key;
-        }
-      }
-      localStorage.setItem("key",key)
-      if(flag){
-        //이미 로그인됨
-        this.firemain.child("users").child(key).update({"last_login":new Date()})
-        this.navCtrl.setRoot(HomePage,{"id":id})
-    }else{
-      //최초 로그인
-      var ab="false";
-      setTimeout(() => {
-        ab =this.certificating(id);
-      }, 1000);
-      
-      
+    localStorage.setItem('loginflag','false');
+
+    if(localStorage.getItem('loginflag')==='true'){
+      // this.main_title='회원가입/로그인';
+      this.login_check=true;
+      this.name=localStorage.getItem('name');
+      this.phone=localStorage.getItem('phone');
+      // this.number=localStorage.getItem('number');
     }
-    })
+    else{
+      this.main_title='회원가입/로그인';
+      this.login_check=false;
+    }
 
-    // window.alert(this.id);
-    // localStorage.setItem("loginflag", String(this.check));
-    // // var user = this.fire.auth.currentUser.email;
-    // this.navCtrl.setRoot(HomePage, { "id": this.id });
+    console.log(this.login_check)
   }
-  certificating(email){
 
-    var userCode = 'imp10391932';
-    var data = {
-      merchant_uid: 'mid_' + new Date().getTime(),
-    };
-    var params = {
-      userCode: userCode,                           // 가맹점 식별코드
-      data: data,                                   // 결제 데이터
-      callback: (response)=> {
-        console.log(response);
-        if(response.imp_success=="true"){
+  goback(){
+    this.phone_check=false;
+    this.login_check=false;
 
+  }
 
-          setTimeout(()=>{
-              var a = this.firemain.child("users").push().key;
-              this.firemain.child("users").child(a).update({"key":a,"email":email,"first_login":new Date()})
-              window.alert("success first login")
-          },500)
+  certified(){
+    if(this.name==='') alert('이름을 입력해주세요.');
+    else if(this.phone==='') alert('휴대폰 번호를 입력해주세요.');
+    else{
+      this.certified_check=true;
+    }
+  }
 
+  login(){
+    this.login2();
+    // if(this.name==='') alert('이름을 입력해주세요.');
+    // else if(this.phone==='') alert('휴대폰 번호를 입력해주세요.');
+    // else if(this.code==='') alert('인증번호를 입력해주세요.');
+    // else{
+    //   this.firemain.child('users2').child(this.phone).once("value",(snap)=>{
+    //     if(snap.val()){
+    //       this.phone_check=true;
+    //       this.main_title="계정 확인"
+    //     }
+    //     else this.login_check=true;
+    //   }).then(()=>{
 
-          return "true";
-        }
+    //     if(this.phone_check===true){
+    //       // alert()
+    //     }
+    //     else{
+    //       this.login2();
+    //     }
+    //   })
+    // }
+  }
 
-      },                           // 콜백 함수
-    };
-    IamportCordova.certification(params);
-    return "false"
+  login2(){
+    this.firemain.child('users2').child(this.phone).update(
+      {
+        'name':this.name,
+        'phone':this.phone,
+        'last_login':new Date(),
+      }
+    )
+    alert('로그인!!!!');
+    this.navCtrl.setRoot(HomePage,{"id":'kotraner88@gmail.com'})
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginpagePage');
-
-
-    // var id = localStorage.getItem("id");
-    // var pass = localStorage.getItem("pass");
-    // console.log(id);
-    // console.log(pass);
-    // if(id=="null"){
-    //   id=""
-    //   this.id="";
-    // }
-    // if(id==null||id==undefined){
-    //   id=""
-    //   this.id="";
-    // }
-    // var idd = id + "";
-    // console.log(idd);
-    // if ((id != "null" && pass != "null" &&id!="")) {
-    //     this.id = idd;
-    //     this.id="";
-    //     this.password = pass;
-    //     console.log("??");
-    //     this.fire.auth.signInWithEmailAndPassword(idd.trim(), pass)
-    //         .then( (data) =>{
-    //         // localStorage.setItem("gisu", this.id.split("기")[0]);
-    //         // localStorage.setItem("name", this.id.split(".")[1].split("@")[0]);
-    //         // localStorage.setItem("pass", this.password);
-    //         // this.navCtrl.setRoot(MainPage, { "id": this.id });
-    //     }).catch( (error)=> {
-    //         console.log(error);
-    //         alert("login failed" + error);
-    //         if (error.code == "auth/user-not-found") {
-    //         }
-    //         else {
-                
-    //         }
-    //         this.id = "";
-    //         this.password = "";
-    //     });
-    // }else{
-    //   console.log("initialize")
-    // }
   }
 }
