874ea94c10174e5757ecb713a3a7f18f425a1ea7
kotran <>
2020-03-28 17:22:02 +0900

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,8 +14,8 @@ import {ChatroomlistPage} from '../pages/chatroomlist/chatroomlist'
   templateUrl: 'app.html'
 })
 export class MyApp {
-  // rootPage:any = HomePage;
-  rootPage:any=ChatroomlistPage;
+  rootPage:any = HomePage;
+  // rootPage:any=ChatroomlistPage;
 
   constructor(platform: Platform/* , statusBar: StatusBar, splashScreen: SplashScreen */) {
     platform.ready().then(() => {
